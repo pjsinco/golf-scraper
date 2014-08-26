0f96663c14e16f88655cb1e1d58fbe81387a0236
Patrick Sinco <psinco@gmail.com>
2014-08-26 07:28:15 -0500

    starts to build up tourney-id-scraper

--- tourney-id-scraper.py
+++ tourney-id-scraper.py
@@ -4,7 +4,52 @@ import time
 import json
 import pprint
 
-base_url = 'http://espn.go.com/golf/leaderboard?tournamentId='
+#base_url = 'http://espn.go.com/golf/leaderboard?tournamentId='
+url = 'http://espn.go.com/golf/schedule'
+
+#set up our dictionary
+tourney = {}
+
+try:
+#  response = urllib2.urlopen(url)
+#  content = response.read()
+#  soup = BeautifulSoup(content)
+  
+#  rows = soup.select('.tablehead tr[class*="row"]');
+
+  row = '\
+    <tr class="oddrow"> \
+      <td><nobr>Aug 29 - Sep 1</nobr></td> \
+      <td><a href="/golf/leaderboard?tournamentId=1343">Deutsche \
+        Bank Championship</a><br><em>TPC of Boston, Norton, MA</em></br> \
+      </td> \
+      <td>NBC/TGC</td> \
+      <td> \
+        <a href="http://espn.go.com/golf/player/_/id/576/henrik-stenson"> \
+          Henrik Stenson \
+        </a> \
+      </td> \
+      <td>$8,000,000</td> \
+    </tr>'
+
+  soup = BeautifulSoup(row)
+  print len(soup.select('td'))
+  print soup.select('td')
+  print soup.select('td')[3]
+  print soup.select('td')[3].get_text()
+
+  #print len(rows)
+  #print type(rows)
+
+  for x in range(0, len(rows)):
+    print x
+
+  #for table in tables:
+  #for x in range(0, len(tables)):
+    #print len(tables[x])
+  
+except:
+  pass
 
 
 
