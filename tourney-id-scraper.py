3b57b7e249839a0a04eac88d849ac97a2f18cc71
pjsinco <psinco@fas.harvard.edu>
2014-08-28 14:31:29 -0500

    gets tourney-id-scraper to grab each tourney name, id

--- tourney-id-scraper.py
+++ tourney-id-scraper.py
@@ -15,56 +15,21 @@ try:
   content = response.read()
   soup = BeautifulSoup(content)
   
-  rows = soup.select('.tablehead tr[class*="row"]');
+  # example target link: 
+  # http://espn.go.com/golf/leaderboard?tournamentId=1319
+  tourney_hrefs = soup.select('a[href*="tournament"]')
 
-  # testing here ------------------
+  tourneys = {}
 
-  # row = '\
-  #   <tr class="oddrow"> \
-  #     <td><nobr>Aug 29 - Sep 1</nobr></td> \
-  #     <td><a href="/golf/leaderboard?tournamentId=1343">Deutsche \
-  #       Bank Championship</a><br><em>TPC of Boston, Norton, MA</em></br> \
-  #     </td> \
-  #     <td>NBC/TGC</td> \
-  #     <td> \
-  #       <a href="http://espn.go.com/golf/player/_/id/576/henrik-stenson"> \
-  #         Henrik Stenson \
-  #       </a> \
-  #     </td> \
-  #     <td>$8,000,000</td> \
-  #   </tr>'
-
-  # soup = BeautifulSoup(row)
-  # print len(soup.select('td'))
-  # print soup.select('td')
-  # print soup.select('td')[3]
-  # print soup.select('td')[3].get_text()
-
-  # finished testing ------------------
-
-  print type(rows)
-  print len(rows)
-
-  #for x in range(0, len(rows)):
-  for row in rows:
-    #print row
-    print 'row is a %s' % type(row)
-    print '\tits name is %s' % row.name
-    print '\tits class is %s' % row['class']
-    print '\tits attrs are: %s' % row.attrs
-    print '\tit has %d <td> elems' % len(row.find_all('td'))
-    print type(row.find_all('td'))
-    print len(row.select('td'))
-    print '\n'
-    #row_soup = BeautifulSoup(row)
-    #print len(row_soup.select('td'))
-
-  #for table in tables:
-  #for x in range(0, len(tables)):
-    #print len(tables[x])
-  
+  for href in tourney_hrefs:
+    tourney = {}
+    tourney['name'] = href.get_text()
+    tourney['id'] = href.get('href').split('=')[-1] # id is last item
+    tourneys[tourney['id']] = tourney
+    
 except:
   pass
 
+print len(tourneys)
 
 
