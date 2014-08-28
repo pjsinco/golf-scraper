4830322ec961577ea763ece9eec239666445c3ce
Patrick Sinco <psinco@gmail.com>
2014-08-28 07:54:04 -0500

    plays around a little more beautifulsoup methods

--- tourney-id-scraper.py
+++ tourney-id-scraper.py
@@ -11,38 +11,53 @@ url = 'http://espn.go.com/golf/schedule'
 tourney = {}
 
 try:
-#  response = urllib2.urlopen(url)
-#  content = response.read()
-#  soup = BeautifulSoup(content)
+  response = urllib2.urlopen(url)
+  content = response.read()
+  soup = BeautifulSoup(content)
   
-#  rows = soup.select('.tablehead tr[class*="row"]');
+  rows = soup.select('.tablehead tr[class*="row"]');
 
-  row = '\
-    <tr class="oddrow"> \
-      <td><nobr>Aug 29 - Sep 1</nobr></td> \
-      <td><a href="/golf/leaderboard?tournamentId=1343">Deutsche \
-        Bank Championship</a><br><em>TPC of Boston, Norton, MA</em></br> \
-      </td> \
-      <td>NBC/TGC</td> \
-      <td> \
-        <a href="http://espn.go.com/golf/player/_/id/576/henrik-stenson"> \
-          Henrik Stenson \
-        </a> \
-      </td> \
-      <td>$8,000,000</td> \
-    </tr>'
+  # testing here ------------------
 
-  soup = BeautifulSoup(row)
-  print len(soup.select('td'))
-  print soup.select('td')
-  print soup.select('td')[3]
-  print soup.select('td')[3].get_text()
+  # row = '\
+  #   <tr class="oddrow"> \
+  #     <td><nobr>Aug 29 - Sep 1</nobr></td> \
+  #     <td><a href="/golf/leaderboard?tournamentId=1343">Deutsche \
+  #       Bank Championship</a><br><em>TPC of Boston, Norton, MA</em></br> \
+  #     </td> \
+  #     <td>NBC/TGC</td> \
+  #     <td> \
+  #       <a href="http://espn.go.com/golf/player/_/id/576/henrik-stenson"> \
+  #         Henrik Stenson \
+  #       </a> \
+  #     </td> \
+  #     <td>$8,000,000</td> \
+  #   </tr>'
 
-  #print len(rows)
-  #print type(rows)
+  # soup = BeautifulSoup(row)
+  # print len(soup.select('td'))
+  # print soup.select('td')
+  # print soup.select('td')[3]
+  # print soup.select('td')[3].get_text()
 
-  for x in range(0, len(rows)):
-    print x
+  # finished testing ------------------
+
+  print type(rows)
+  print len(rows)
+
+  #for x in range(0, len(rows)):
+  for row in rows:
+    #print row
+    print 'row is a %s' % type(row)
+    print '\tits name is %s' % row.name
+    print '\tits class is %s' % row['class']
+    print '\tits attrs are: %s' % row.attrs
+    print '\tit has %d <td> elems' % len(row.find_all('td'))
+    print type(row.find_all('td'))
+    print len(row.select('td'))
+    print '\n'
+    #row_soup = BeautifulSoup(row)
+    #print len(row_soup.select('td'))
 
   #for table in tables:
   #for x in range(0, len(tables)):
