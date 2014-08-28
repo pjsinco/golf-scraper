7b014f7cca8ea632f4410a1fb44786299e1bf015
Patrick Sinco <psinco@gmail.com>
2014-08-28 18:24:34 -0500

    collects tournament ids in tourney-ids.json

--- tourney-id-scraper.py
+++ tourney-id-scraper.py
@@ -5,31 +5,38 @@ import json
 import pprint
 
 #base_url = 'http://espn.go.com/golf/leaderboard?tournamentId='
-url = 'http://espn.go.com/golf/schedule'
+url = 'http://espn.go.com/golf/schedule/_/year/'
 
-#set up our dictionary
-tourney = {}
+year_range = range(2001, 2013)
+year_range.append('2013-14')
+years = []
+for y in year_range:
+  years.append(str(y)) # turn the ints into strings
 
-try:
-  response = urllib2.urlopen(url)
-  content = response.read()
-  soup = BeautifulSoup(content)
-  
-  # example target link: 
-  # http://espn.go.com/golf/leaderboard?tournamentId=1319
-  tourney_hrefs = soup.select('a[href*="tournament"]')
+tourneys_all = {} 
 
-  tourneys = {}
+for year in years:
+  try:
+    tourneys_year = {}
+    response = urllib2.urlopen(url + year)
+    content = response.read()
+    soup = BeautifulSoup(content)
 
-  for href in tourney_hrefs:
-    tourney = {}
-    tourney['name'] = href.get_text()
-    tourney['id'] = href.get('href').split('=')[-1] # id is last item
-    tourneys[tourney['id']] = tourney
+    # example target link: 
+    # http://espn.go.com/golf/leaderboard?tournamentId=1319
+    tourney_hrefs = soup.select('a[href*="tournament"]')
     
-except:
-  pass
+    for href in tourney_hrefs:
+      tourney_name = href.get_text()
+      tourney_id  = href.get('href').split('=')[-1] # id is last item
+      tourneys_year[tourney_id] = tourney_name
+    
+  except:
+    pass
 
-print len(tourneys)
+  finally:
+    tourneys_all[year] = tourneys_year
+    with open ('tourney-ids.json', 'w') as outfile:
+      json.dump(tourneys_all, outfile)
 
 
